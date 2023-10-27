c1d2238609ba2ad47b7ca33ac01176f7ca566735
Ilya <tyulin600@gmail.com>
2023-10-27 17:43:02 +0500

    made core functions

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ const list = document.querySelector('.main__list');
 const input = document.querySelector('.main__input');
 let number = 0;
 
-
-
-input.addEventListener('input', async function () {
-    number = input.value;
-    removeCurrencies();
-    //await addCurrencies(number);
-});
-
 const promise = fetch('https://www.cbr-xml-daily.ru/daily_json.js')
     .then((response) => response.json())
     .then((data) => {
@@ -17,7 +9,7 @@ const promise = fetch('https://www.cbr-xml-daily.ru/daily_json.js')
         for (item in data.Valute) {
             currencies.push({
                 name: data.Valute[item].Name !== 'СДР (специальные права заимствования)' ? data.Valute[item].Name : 'СДР',
-                price: data.Valute[item].Value / data.Valute[item].Nominal
+                price: (data.Valute[item].Value / data.Valute[item].Nominal).toFixed(2),
             });
         }
         return currencies;
@@ -25,22 +17,33 @@ const promise = fetch('https://www.cbr-xml-daily.ru/daily_json.js')
 
 async function addCurrencies(number = 0) {
     const result = await promise;
-    for (item of result) {
-        const newItem = document.createElement('li');
-        newItem.classList.add('main__list-item');
-        newItem.innerHTML = `<p>Можно купить ${(number / item.price).toFixed(2)} ${item.name}, имея ${number} руб., по курсу ${item.price} руб. за единицу.</p>`
-        list.appendChild(newItem);
+    if (number) {
+        for (item of result) {
+            const newItem = document.createElement('li');
+            newItem.classList.add('main__list-item');
+            newItem.innerHTML = `<p>Можно купить ${(number / item.price).toFixed(2)} ${item.name}, имея ${number} руб., по курсу ${item.price} руб. за единицу.</p>`
+            list.appendChild(newItem);
+        }
+    } else {
+        for (item of result) {
+            const newItem = document.createElement('li');
+            newItem.classList.add('main__list-item');
+            newItem.innerHTML = `<p>1 ${item.name} стоит ${item.price} руб.</p>`
+            list.appendChild(newItem);
+        }
     }
 }
 
 function removeCurrencies() {
-    console.log(list.children)
-    for (let i = 0; i < Array.from(list.children).length; i++) {
-        console.log(i)
-        list.removeChild(Array.from(list.children)[i]);
-        
+    for (item of Array.from(list.children)) {
+        list.removeChild(item);
     }
-    
 }
+
+input.addEventListener('input', function () {
+    number = input.value;
+    removeCurrencies();
+    addCurrencies(number);
+});
 
 addCurrencies()
